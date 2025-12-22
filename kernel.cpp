c2f9d6896248edbefe9bbfebec0a65f1efc3e201
Tedi-Company <lelecanalfahl@gmail.com>
2025-12-22 11:06:18 -0300

    Add printf function and kernel main entry point
    Implement basic printf function for kernel output.

---
+++ kernel.cpp
@@ -0,0 +1,12 @@
+void printf(char* str){
+  unsigned short* VideoMemory = (unsigned short*)0xb8000;
+
+  for(int i = 0; str[i] != '\0'; ++i){
+    VideoMemory[i] = (VideoMemory[i] & 0xFF00) / str[i];
+  }
+}
+
+extern "C" void kmain(){
+  printf("Hello, You are at IATC! Credits: Kernel and project based on that of https://terminalroot.com.br/ text translated from Portuguese by Google Translate");
+  while(1);
+}
